63b448998ec35222473ebda02804d57193bcd139
DesarrolladorJuanCTorres <jctorresdelcastillo@gmail.com>
2025-07-15 01:53:52 -0500

    cambios varios

--- resources/views/facturacion.blade.php
+++ resources/views/facturacion.blade.php
@@ -155,7 +155,7 @@
     <!-- About End -->
 
     <!-- Feature Start -->
-    <div class="container-fluid feature bg-light">
+    <div class="container-fluid feature bg-light py-5">
         <div class="container">
             <div class="text-center mx-auto wow fadeInUp" data-wow-delay="0.2s" style="max-width: 800px;">
                 <!-- <h4 class="text-primary">Nuestros Servicios</h4> -->
@@ -266,6 +266,46 @@
         </div>
     </div>
     <!-- Feature End -->
+
+    <!-- PRECIOS -->
+    <div class="pricingTable">
+        <h2 class="pricingTable-title">Find a plan that's right for you.</h2>
+        <h3 class="pricingTable-subtitle">Every plan comes with a 30-day   free trial.</h3>
+        
+        <ul class="pricingTable-firstTable">
+            <li class="pricingTable-firstTable_table">
+            <h1 class="pricingTable-firstTable_table__header">Bronze Packege</h1>
+            <p class="pricingTable-firstTable_table__pricing"><span>$</span><span>10</span><span>Month</span></p>
+            <ul class="pricingTable-firstTable_table__options">
+                <li>Unlimited Listing</li>
+                <li>Edit Your Listing</li>
+                <li>Approve Reviews</li>
+            </ul>
+            <button class="pricingTable-firstTable_table__getstart">Get Started Now</button>
+            </li><li class="pricingTable-firstTable_table">
+            <h1 class="pricingTable-firstTable_table__header">Gold Packege</h1>
+            <p class="pricingTable-firstTable_table__pricing"><span>$</span><span>19</span><span>Month</span></p>
+            <ul class="pricingTable-firstTable_table__options">
+                <li>Unlimited Listing</li>
+                <li>Edit Your Listing</li>
+                <li>Approve Reviews</li>
+                <li>Take Booking Online</li>
+                <li>24/7 Support Service</li>
+            </ul>
+            <button class="pricingTable-firstTable_table__getstart">Get Started Now</button>
+            </li><li class="pricingTable-firstTable_table">
+            <h1 class="pricingTable-firstTable_table__header">Platinum Packege</h1>
+            <p class="pricingTable-firstTable_table__pricing"><span>$</span><span>49</span><span>Month</span></p>
+            <ul class="pricingTable-firstTable_table__options">
+                <li>Unlimited Listing</li>
+                <li>Edit Your Listing</li>
+                <li>Approve Reviews</li>
+            </ul>
+            <button class="pricingTable-firstTable_table__getstart">Get Started Now</button>
+            </li>
+        </ul>
+    </div>
+    <!-- END PRECIOS -->
 
     <!-- FAQs Start -->
     <div class="container-fluid faq-section bg-light py-5">
