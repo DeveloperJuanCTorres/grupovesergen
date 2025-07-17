82c7bac18787dbbf0f75916185f7f4a0a664efa1
DesarrolladorJuanCTorres <jctorresdelcastillo@gmail.com>
2025-07-16 23:58:40 -0500

    cambios

--- resources/views/product-list.blade.php
+++ resources/views/product-list.blade.php
@@ -5,17 +5,17 @@
 @foreach($products as $product)
 <div class="col-lg-4 col-md-6 col-sm-6 pb-1">
     <div class="product-item bg-white mb-4" style="border-radius: 15px;">
-        <div class="product-img position-relative overflow-hidden">
+        <div class="product-img position-relative overflow-hidden bg-light">
             @php
                 $imagenes = json_decode($product->images)
             @endphp
             @if($imagenes)
             <a href="{{route('product.detail', $product)}}">
-                <img class="img-fluid w-100" src="storage/{{$imagenes[0]}}" alt="">
+                <img class="img-fluid w-100" style="height: 200px;" src="storage/{{$imagenes[0]}}" alt="">
             </a>
             @else
             <a href="{{route('product.detail', $product)}}">
-                <img class="img-fluid w-100" src="img/defectomaster.png" alt="">
+                <img class="img-fluid d-block m-auto" src="img/defectomaster.png" alt="">
             </a>
             @endif
         </div>
@@ -27,12 +27,12 @@
         </div>
         <div class="product-action text-center pb-4 px-4 w-100">
             <input type="hidden" id="qty" value="1">
-            <a class="btn btn-outline-dark addcart w-100" href="#" data-id="{{$product->id}}">
+            <a class="btn btn-primary addcart w-100" href="#" data-id="{{$product->id}}">
                 <i class="fa fa-shopping-cart"></i>
                 Agregar al carrito
             </a>
 
-            <div class="d-flex align-items-center justify-content-center mb-1">
+            <div class="d-flex align-items-center justify-content-center mt-2">
                 <small>Stock ({{$product->stock}} {{$product->unidad_medida}})</small>
             </div>
         </div>
