d4941f5e90c32527e186eafe69f598df99d496db
DesarrolladorJuanCTorres <jctorresdelcastillo@gmail.com>
2025-08-05 18:57:41 -0500

    cambios

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,7 +17,7 @@
     <link href="img/favicon.ico" rel="icon">
 
     <?php
-        $version = '1993.2.7';
+        $version = '1993.2.8';
     ?>
 
     <script src="assets/js/plugin/webfont/webfont.min.js"></script>
