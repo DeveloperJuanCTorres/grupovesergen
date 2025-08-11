1aa5adb15b2f59ebe82785cdfdaa9eda824aeea7
DesarrolladorJuanCTorres <jctorresdelcastillo@gmail.com>
2025-08-11 18:51:22 -0500

    stilos

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,7 +17,7 @@
     <link href="img/favicon.ico" rel="icon">
 
     <?php
-        $version = '1993.3.1';
+        $version = '1993.3.2';
     ?>
 
     <script src="assets/js/plugin/webfont/webfont.min.js"></script>
