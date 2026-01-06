d4a0135ff747e400cb79a5f28623dbce7d01ecfa
Juan Carlos Torres del Castillo <jctorresdelcastillo@gmail.com>
2026-01-06 12:45:32 -0500

    cambios

--- resources/views/services.blade.php
+++ resources/views/services.blade.php
@@ -116,8 +116,8 @@
                         <div class="service-content p-4">
                             <div class="service-content-inner">
                                 <a href="#" class="d-inline-block h4">{{$service->title}}</a>
-                                <p class="">{{$service->subtitle}}</p>
-                                <a class="btn btn-primary rounded-pill py-2 px-4" href="{{route('service.detail', $service)}}">Leer más</a>
+                                <p class="text-center">{{$service->subtitle}}</p>
+                                <a class="btn btn-primary rounded-pill py-2 px-4 text-center" href="{{route('service.detail', $service)}}">Leer más</a>
                             </div>
                         </div>
                     </div>
