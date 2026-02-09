a2f9a791f1433487a31a40dbb49d9ab30db80e31
Juan Carlos Torres del Castillo <jctorresdelcastillo@gmail.com>
2026-02-09 12:10:09 -0500

    cambios2

--- resources/views/product-detail.blade.php
+++ resources/views/product-detail.blade.php
@@ -323,7 +323,7 @@
     <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 	<script src="{{asset('js/addcart.js')}}"></script>
 
-    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"> -->
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"> -->   
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 
