c5d533738a69f1517623206d6b8a1fb3b2fdf239
Juan Carlos Torres del Castillo <jctorresdelcastillo@gmail.com>
2026-02-11 12:03:12 -0500

    style

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,7 +17,7 @@
     <link href="img/favicon.ico" rel="icon">
 
     <?php
-        $version = '1993.3.7';
+        $version = '1993.3.8';
     ?>
 
     <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('logos/16X16.png') }}?v=<?php echo $version ?>">
