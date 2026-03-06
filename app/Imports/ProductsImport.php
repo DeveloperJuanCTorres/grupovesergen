54e47091d927344db48b362f0ac30b690da762e2
Juan Carlos Torres del Castillo <jctorresdelcastillo@gmail.com>
2026-03-06 14:39:13 -0500

    listos

--- app/Imports/ProductsImport.php
+++ app/Imports/ProductsImport.php
@@ -6,28 +6,52 @@ use App\Models\Product;
 use Illuminate\Support\Str;
 use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
+use Maatwebsite\Excel\Concerns\WithEvents;
+use Maatwebsite\Excel\Events\BeforeImport;
 
-class ProductsImport implements ToModel, WithHeadingRow
+class ProductsImport implements ToModel, WithHeadingRow, WithEvents
 {
+    protected $proveedor_id;
+
+    public function __construct($proveedor_id)
+    {
+        $this->proveedor_id = $proveedor_id;
+    }
+
+    public function registerEvents(): array
+    {
+        return [
+            BeforeImport::class => function () {
+
+                // 🔴 Poner stock en 0 a todos los productos del proveedor
+                Product::where('proveedor_id', $this->proveedor_id)
+                    ->update(['stock' => 0]);
+
+            },
+        ];
+    }
+
     public function model(array $row)
     {
+
         $product = Product::where('codigo_interno_proveedor', $row['codigo_interno_proveedor'])
+        ->where('proveedor_id', $this->proveedor_id)
         ->first();
 
         if ($product) {
 
-            // 🔄 Solo actualizar campos específicos
+            // 🔄 actualizar
             $product->update([
                 'price' => $row['price'],
                 'price_tecnico' => $row['price_tecnico'],
                 'stock' => $row['stock'],
             ]);
 
-            return $product;
+            return null;
 
         } else {
 
-            // ➕ Crear nuevo producto
+            // ➕ crear
             return new Product([
                 'name' => $row['name'],
                 'price' => $row['price'],
@@ -37,7 +61,7 @@ class ProductsImport implements ToModel, WithHeadingRow
                 'slug' => Str::slug($row['name']),
                 'codigo' => $row['codigo'],
                 'codigo_interno_proveedor' => $row['codigo_interno_proveedor'],
-                'proveedor_id' => $row['proveedor_id'],
+                'proveedor_id' => $this->proveedor_id,
                 'condition_id' => $row['condition_id'],
             ]);
         }
